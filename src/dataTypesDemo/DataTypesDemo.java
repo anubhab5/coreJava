3e4d61ce3703355e2fe6d4d3d18319fda4fbb0cb
Anubhab Mukherjee <anubhabmukherjee5@gmail.com>
2020-03-31 15:43:03 +0530

    datat Types in Java

---
+++ src/dataTypesDemo/DataTypesDemo.java
@@ -0,0 +1,37 @@
+package dataTypesDemo;
+
+public class DataTypesDemo {
+
+	public static void main(String[] args) {
+
+		// 1 byte
+		byte lowerLimitByte = -128;
+		byte higherLimitByte = 127;
+
+		// 2 bytes
+		short lowerLimitShort = -32768;
+		short upperLimitShort = -32767;
+
+		// 4 bytes
+		int lowerLimitInt = -2147483648;
+		int upperLimitInt = 2147483647;
+
+		// 8 bytes
+		long lowerLimitLong = -9223372036854775807l;
+		long upperLimitLong = 9223372036854775806l;
+		
+		// 2 bytes
+		char charLowerLimit = 0;
+		char charUpperLimit = 65535;
+		
+		// 4 bytes
+		float floatVal = -32769.67f;
+		
+		double dowubleVal = 88d;
+		
+		boolean boolTrueVal = true;
+		boolean boolfalseVal = false;
+
+	}
+
+}
