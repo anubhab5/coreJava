164b0bf4a5c51e232b270629ee27b206b583a550
Anubhab Mukherjee <anubhabmukherjee5@gmail.com>
2020-03-31 22:16:47 +0530

    Assignment 1

---
+++ src/dataTypesDemo/Assigment1.java
@@ -0,0 +1,18 @@
+package dataTypesDemo;
+
+public class Assigment1 {
+
+	public static void main(String[] args) {
+
+		int integerValue = 10;		
+		float floatValue = 20.54f;		
+		char charValue = 'X';		
+		String stringValue = "I am the best";
+		
+		System.out.println(integerValue);
+		System.out.println(floatValue);
+		System.out.println(charValue);
+		System.out.println(stringValue);
+	}
+
+}
