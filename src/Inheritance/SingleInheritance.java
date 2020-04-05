fcbcc946bdae7e112cde542c01a56a6a237ed86a
Anubhab Mukherjee <anubhabmukherjee5@gmail.com>
2020-04-05 19:25:38 +0530

    Inheritance Demo

---
+++ src/Inheritance/SingleInheritance.java
@@ -0,0 +1,16 @@
+package Inheritance;
+
+public class SingleInheritance {
+
+	public static void main(String[] args) {
+
+		SingleInheritance si = new SingleInheritance();
+		si.testMethod();
+		si.hashCode();
+		si.getClass();
+	}
+
+	void testMethod() {
+
+	}
+}
