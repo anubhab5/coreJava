f4e2f7b66c753fb20f4cacb324d1faf92c4f706f
Anubhab Mukherjee <anubhabmukherjee5@gmail.com>
2020-05-11 22:40:16 +0530

    Inner Static Class

---
+++ src/innerClasses/Outer.java
@@ -0,0 +1,21 @@
+package innerClasses;
+
+public class Outer {
+
+	static void f1() {
+		System.out.println("Outers static method.");
+	}
+
+	static class Inner {
+
+		static void f2() {
+			System.out.println("Inners static method");
+		}
+	}
+
+	public static void main(String[] args) {
+		Outer.f1();
+		Outer.Inner.f2();
+	}
+
+}
